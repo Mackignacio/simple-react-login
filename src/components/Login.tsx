7f7d99316f8f37fb3df3cd2ba395a5a434634295
Mack Ignacio <mack_ignacio@yahoo.com>
2020-12-26 13:48:21 +0800

    Convert Login to use hooks

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { UserLoginData, UserData } from "../mock/User.data";
+import { useState } from "react";
+import { UserData } from "../mock/User.data";
 import { userLogin } from "../services/User.services";
 
 interface LoginProps {
@@ -7,61 +7,47 @@ interface LoginProps {
   setLogin: (status: boolean, user: UserData) => void;
 }
 
-export default class Login extends React.Component<LoginProps, UserLoginData> {
-  state = {
-    email: "",
-    password: "",
-  };
+const Login = (props: LoginProps) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  onSubmit = (event: any) => {
+  const onSubmit = (event: any) => {
     event.preventDefault();
-    const user = userLogin(this.state);
+    const user = userLogin({ email, password });
 
     if (!user) {
-      this.props.setAlert(true, "Email or Password is incorrect!");
+      props.setAlert(true, "Email or Password is incorrect!");
       return;
     }
 
-    this.props.setLogin(true, user);
+    props.setLogin(true, user);
   };
 
-  passwordOnChange = (event: any) => {
-    this.setState({ password: event.target.value });
+  const passwordOnChange = (event: any) => {
+    setPassword(event.target.value);
   };
 
-  emailOnChange = (event: any) => {
-    this.setState({ email: event.target.value });
+  const emailOnChange = (event: any) => {
+    setEmail(event.target.value);
   };
 
-  render() {
-    return (
-      <div>
-        <form>
-          <div className="form-group">
-            <label>Email address</label>
-            <input
-              type="email"
-              className="form-control col-md-6"
-              placeholder="Enter email"
-              onChange={this.emailOnChange}
-              value={this.state.email}
-            />
-          </div>
-          <div className="form-group">
-            <label>Password</label>
-            <input
-              type="password"
-              className="form-control col-md-6"
-              placeholder="Password"
-              onChange={this.passwordOnChange}
-              value={this.state.password}
-            />
-          </div>
-          <button type="submit" className="btn btn-primary" onClick={this.onSubmit}>
-            Login
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <form>
+        <div className="form-group">
+          <label>Email address</label>
+          <input type="email" className="form-control col-md-6" placeholder="Enter email" onChange={emailOnChange} value={email} />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input type="password" className="form-control col-md-6" placeholder="Password" onChange={passwordOnChange} value={password} />
+        </div>
+        <button type="submit" className="btn btn-primary" onClick={onSubmit}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
